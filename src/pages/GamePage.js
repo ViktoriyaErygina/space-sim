15303a67282df68fd60f90dd8313f54b4395d2aa
Viktoriya Erygina <yerygina@indorsoft.ru>
2024-09-23 03:33:01 +0700

    вроде закончена 2 лаба

--- src/pages/GamePage.js
+++ src/pages/GamePage.js
@@ -1,6 +1,9 @@
 import {useLocation, useNavigate} from "react-router-dom";
+import playerImg from '../content/Hero_Ship.png'
+import meteoriteImg from '../content/Meteorite.png'
+import heroShorImg from '../content/Hero_Shot.png'
 import {CommonButton, GameArea, GameInfo, GameProcessPanel, Wrapper} from "./components/styles";
-import {useState} from "react";
+import {useRef, useState} from "react";
 
 export default function GamePage() {
     const location = useLocation()
@@ -19,10 +22,49 @@ export default function GamePage() {
         )
     }
 
+    const playerRef = useRef()
+
+    const [state, setState] = useState({
+        hero: {
+            left: 0, top: 0, wasShot: false
+        },
+    })
+
     return (
         <div>
             <Wrapper>
-                <GameArea />
+                <GameArea>
+                    <img src={playerImg} alt="player" ref={playerRef}
+                         style={{
+                             bottom: "20px",
+                             left: "50%",
+                             position: "absolute",
+                             width: 60,
+                             height: 100
+                         }}/>
+                    >
+
+                    <img src={meteoriteImg} alt="meteorite"
+                         style={{
+                             left: "50%",
+                             top: "20px",
+                             position: "absolute",
+                             width: 170,
+                             height: 140
+                         }}/>
+                    >
+
+                    <img src={heroShorImg} alt="hero-shot"
+                         style={{
+                             left: "50%",
+                             bottom: "140px",
+                             position: "absolute",
+                             width: 15,
+                             height: 45
+                         }}/>
+                    >
+                </GameArea>
+
                 <GameProcessPanel>
                     <GameInfo>Таймер: {data.paramTime.minutes}:{data.paramTime.seconds}</GameInfo>
                     <GameInfo>Баллы: {score}</GameInfo>
