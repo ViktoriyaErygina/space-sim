9e9fc317a24ca04ba496e49c3b069cfe4050b225
Ilya Shishkin <mostbook@mail.ru>
2024-10-21 17:16:10 +0700

    Добавлено перемещение метеоритов под углом

--- src/pages/components/MovingObject.js
+++ src/pages/components/MovingObject.js
@@ -1,22 +1,28 @@
-import {useEffect, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import mateoriteImg from '../../content/Meteorite.png'
 
-
 export default function MovingObject() {
-    const [top, setTop] = useState(-50)
-    const [left, setLeft] = useState(Math.random() * window.innerWidth)
+    const [top, setTop] = useState()
+    const [left, setLeft] = useState()
+    const angleRef = useRef((Math.random() * 180) * Math.PI / 180);
+    const timeRef = useRef(0);
+    const initLeft = useRef(Math.random() * window.innerWidth);
 
     useEffect(() => {
         const moveObject = setInterval(() => {
-            setTop(prevTop => prevTop + 1)
-            if (top > window.innerHeight) {
-                setTop(-50)
-                setLeft(Math.random() * window.innerWidth)
+            setTop(Math.sin(angleRef.current) * timeRef.current - 100);
+            setLeft(initLeft.current + Math.cos(angleRef.current) * timeRef.current);
+            timeRef.current += 1
+
+            if (top > window.innerHeight || left > window.innerWidth) {
+                timeRef.current = 0;
+                angleRef.current = (Math.random() * 180) * Math.PI / 180;
+                initLeft.current = Math.random() * window.innerWidth;
             }
-        }, 10)
+        }, 14)
 
         return () => clearInterval(moveObject)
-    }, [top])
+    }, [top, left])
 
     return (
         <img src={mateoriteImg} alt={"meteorite"}
